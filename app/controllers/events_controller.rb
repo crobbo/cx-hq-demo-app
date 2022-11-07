0495f0ffa9ab47a749df4430a7f253bcb21474cf
Christian Robinson <christian@robbo.dev>
2022-11-07 21:18:26 +0000

    test remove of index methods

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,12 +3,12 @@ class EventsController < ApplicationController
   before_action :set_statistics, only: :index
 
   def index
-    @total_negatives = @statistics.percentage_negative_current_month
-    @negatives_percentage_change = @statistics.negatives_percentage_change
-    @total_positives = @statistics.percentage_positive_current_month
-    @positives_percentage_change = @statistics.positives_percentage_change
-    @estimated_nps = @statistics.estimated_nps
-    @nps_score_change = @statistics.nps_score_change
+    # @total_negatives = @statistics.percentage_negative_current_month
+    # @negatives_percentage_change = @statistics.negatives_percentage_change
+    # @total_positives = @statistics.percentage_positive_current_month
+    # @positives_percentage_change = @statistics.positives_percentage_change
+    # @estimated_nps = @statistics.estimated_nps
+    # @nps_score_change = @statistics.nps_score_change
   end
 
   private
