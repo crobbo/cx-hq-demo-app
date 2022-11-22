b67245d05b7c5adedcc42df698172b14dd7118ca
Christian Robinson <christian@robbo.dev>
2022-11-22 21:48:40 +0000

    changed google service account auth to use new account details in environment variable.

--- app/services/google_manager/sentiment_checker.rb
+++ app/services/google_manager/sentiment_checker.rb
@@ -8,9 +8,6 @@ module GoogleManager
     end
 
     def call
-      Google::Cloud::Language.configure do |config|
-        config.credentials = "private-key.json"
-      end
       client = Google::Cloud::Language.language_service
       score = client.analyze_sentiment(document: { content: @text, type: :PLAIN_TEXT }).document_sentiment.score
       return false unless score
